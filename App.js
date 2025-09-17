f7a4255629d751fb6efdaf7e38bc795e49296567
Idrus <idrus99@gmail.com>
2025-09-17 09:36:01 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Quran from './pages/Quran';
-import Prophets from './pages/Prophets';
 import QuranViewer from './components/QuranViewer';
 import ProphetStory from './components/ProphetStory';
 import TafsirViewer from './components/TafsirViewer';
+import DiscussionForum from './components/DiscussionForum';
 import './App.css';
 
 function App() {
@@ -12,13 +11,14 @@ function App() {
     <Router>
       <div className="App">
         <nav>
-          <a href="/">Home</a> | <a href="/quran">Al-Qur'an</a> | <a href="/prophets">Kisah Nabi</a> | <a href="/tafsir">Tafsir</a>
+          <a href="/">Home</a> | <a href="/quran">Al-Qur'an</a> | <a href="/prophets">Kisah Nabi</a> | <a href="/tafsir">Tafsir</a> | <a href="/discussions">Forum Diskusi</a>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/quran" element={<QuranViewer />} />
           <Route path="/prophets" element={<ProphetStory />} />
           <Route path="/tafsir" element={<TafsirViewer />} />
+          <Route path="/discussions" element={<DiscussionForum />} />
         </Routes>
       </div>
     </Router>
