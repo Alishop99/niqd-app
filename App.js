de7e16086053a37e2cd7239bfa990af0a644660c
Idrus <idrus99@gmail.com>
2025-09-17 09:29:13 +0700

    Update App.js

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Quran from './pages/Quran';
 import Prophets from './pages/Prophets';
+import QuranViewer from './components/QuranViewer';
+import ProphetStory from './components/ProphetStory';
+import TafsirViewer from './components/TafsirViewer';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        {/* Navbar */}
         <nav>
-          <a href="/">Home</a> | <a href="/quran">Al-Qur'an</a> | <a href="/prophets">Kisah Nabi</a>
+          <a href="/">Home</a> | <a href="/quran">Al-Qur'an</a> | <a href="/prophets">Kisah Nabi</a> | <a href="/tafsir">Tafsir</a>
         </nav>
+
+        {/* Routing */}
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/quran" component={Quran} />
-          <Route path="/prophets" component={Prophets} />
+          <Route path="/quran" component={QuranViewer} />
+          <Route path="/prophets" component={ProphetStory} />
+          <Route path="/tafsir" component={TafsirViewer} />
         </Switch>
       </div>
     </Router>
